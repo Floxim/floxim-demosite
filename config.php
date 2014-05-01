3d78e46473045524ddbb109946c0b872eeefe8a3
Ilya Myasin <dubr.cola@gmail.com>
2014-05-01 20:16:48 +0400

    compiled assets now in repo

--- config.php
+++ config.php
@@ -1,2 +1,10 @@
 <?php
-return false;
+define("FX_JQUERY_PATH", '/floxim/lib/js/jquery-1.9.1.min.js');
+define("FX_JQUERY_UI_PATH", '/floxim/lib/js/jquery-ui-1.10.3.custom.min.js');
+return array(
+    'DB_DSN' => 'mysql:dbname=fx_loc;host=127.0.0.1',
+    'DB_USER' => 'root',
+    'DB_PASSWORD' => '',
+    'IS_DEV_MODE' => false,
+    'COMPILED_TEMPLATES_TTL' => 0
+);
