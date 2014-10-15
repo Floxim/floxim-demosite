dbe4051827620b7945f833c21dcec00ca31af069
Ilya Myasin <dubr.cola@gmail.com>
2014-10-15 13:25:04 +0400

    1. fx 2. buggy inArray fixed

--- module/Floxim/Main/Vacancy/Entity.php
+++ module/Floxim/Main/Vacancy/Entity.php
@@ -7,7 +7,7 @@ class Entity extends \Floxim\Main\Page\Entity {
     public function getFormFields() {
         $fields = fx::collection(parent::getFormFields());
         $fields->apply(function($f) {
-            if (inArray($f['name'], array('salary_from', 'salary_to', 'currency', 'image'))) {
+            if (in_array($f['name'], array('salary_from', 'salary_to', 'currency', 'image'))) {
                 $f['tab'] = 3;
             } elseif ($f['name'] === 'description') {
                 $f['tab'] = 1;
